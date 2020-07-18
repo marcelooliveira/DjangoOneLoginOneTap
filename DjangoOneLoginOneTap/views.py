1ae21cfc6e163461193217487dc5df6245ed2d2b
marcelooliveira <mclricardo@gmail.com>
2020-07-17 23:06:59 -0300

    require_http_methods

--- DjangoOneLoginOneTap/views.py
+++ DjangoOneLoginOneTap/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 import datetime
 import mysecrets
 
 def index(request):
     return render(request, 'index.html', {'one_tap_client_id': mysecrets.ONE_TAP_CLIENT_ID})
 
+
+@require_http_methods(["POST"])
 def one_tap_login(request):
     return HttpResponse()
