571919b99958b5415214a2e254e4bdc6ea8d1c94
marcelooliveira <mclricardo@gmail.com>
2020-07-17 22:12:16 -0300

    One Tap configuration

--- DjangoOneLoginOneTap/views.py
+++ DjangoOneLoginOneTap/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
+import mysecrets
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'one_tap_client_id': mysecrets.ONE_TAP_CLIENT_ID})
