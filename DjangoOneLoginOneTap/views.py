9f10448def43d36b58180bec5c2c05d723affaa8
marcelooliveira <mclricardo@gmail.com>
2020-07-17 22:07:09 -0300

    templates folder

--- DjangoOneLoginOneTap/views.py
+++ DjangoOneLoginOneTap/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import datetime
 
 def index(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render(request, 'index.html')
