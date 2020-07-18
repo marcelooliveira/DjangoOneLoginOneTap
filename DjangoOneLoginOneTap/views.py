f1b031e8fc5c004eab6d054eaf8bbb6bb52c8e4e
marcelooliveira <mclricardo@gmail.com>
2020-07-17 23:25:19 -0300

    Welcome, user

--- DjangoOneLoginOneTap/views.py
+++ DjangoOneLoginOneTap/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 import datetime
 import mysecrets
+import json
+import jwt
 
 def index(request):
     return render(request, 'index.html', {'one_tap_client_id': mysecrets.ONE_TAP_CLIENT_ID})
 
-
 @require_http_methods(["POST"])
 def one_tap_login(request):
-    return HttpResponse()
+    received_json_data = json.loads(request.body)
+    credential = received_json_data['credential']
+    decoded = jwt.decode(credential, verify=False)  
+    return HttpResponse(content=decoded['name'])
