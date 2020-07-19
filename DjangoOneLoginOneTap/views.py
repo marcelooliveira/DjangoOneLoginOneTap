1ac3c71f9a3563ee8d05521d2ce3d705df3843c5
marcelooliveira <mclricardo@gmail.com>
2020-07-19 16:26:07 -0300

    @csrf_exempt decorator on the endopint

--- DjangoOneLoginOneTap/views.py
+++ DjangoOneLoginOneTap/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 from onelogin.saml2.auth import OneLogin_Saml2_Auth
 from onelogin.saml2.settings import OneLogin_Saml2_Settings
@@ -22,6 +23,7 @@ def logout(request):
     request.session['user_data'] = None
     return HttpResponseRedirect('/')
 
+@csrf_exempt
 @require_http_methods(["POST"])
 def one_tap_login(request):
     received_json_data = json.loads(request.body)
